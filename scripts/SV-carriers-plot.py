61314914b9e5623bb1c2f4d746e88bd753781dc6
wdecoster <decosterwouter@gmail.com>
2018-03-19 22:31:47 +0100

    plotting histogram of number of carriers

---
+++ scripts/SV-carriers-plot.py
@@ -0,0 +1,19 @@
+from cyvcf2 import VCF
+import sys
+import matplotlib.pyplot as plt
+
+
+def main(vcf, output):
+    plt.hist(x=[get_carrier_number(v.gt_types) for v in VCF(vcf)],
+             bins=range(10))
+    plt.xlabel('Number of carriers')
+    plt.ylabel('Number of variants')
+    plt.savefig(output)
+
+
+def get_carrier_number(gt_array):
+    return sum(gt_array > 0)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
