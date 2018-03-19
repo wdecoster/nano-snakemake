a0d1749a3ce8b904cb1742efb3a4cafa1a15d0c5
wdecoster <decosterwouter@gmail.com>
2018-03-19 16:51:30 +0100

    max length SV = 2000bp

--- scripts/SV-length-plot.py
+++ scripts/SV-length-plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 plt.hist(x=[v.INFO.get('SVLEN') for v in VCF(sys.argv[1]) if not v.INFO.get('SVTYPE') == 'TRA'],
-         bins=[i for i in range(0, 5000, 50)])
+         bins=[i for i in range(0, 2000, 50)])
 plt.xlabel('Lenghth of structural variant')
 plt.ylabel('Number of variants')
 plt.savefig("SV-length.png")
