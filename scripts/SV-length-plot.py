cce1ba9bf4c87d1e7a935ae14304c719e0e3211e
wdecoster <decosterwouter@gmail.com>
2018-03-19 22:22:43 +0100

    attempting forgetting config file now ignored

--- scripts/SV-length-plot.py
+++ scripts/SV-length-plot.py
@@ -3,8 +3,13 @@ import sys
 import matplotlib.pyplot as plt
 
 
-plt.hist(x=[v.INFO.get('SVLEN') for v in VCF(sys.argv[1]) if not v.INFO.get('SVTYPE') == 'TRA'],
-         bins=[i for i in range(0, 2000, 50)])
-plt.xlabel('Lenghth of structural variant')
-plt.ylabel('Number of variants')
-plt.savefig(sys.argv[2])
+def main(vcf):
+    plt.hist(x=[v.INFO.get('SVLEN') for v in VCF(vcf) if not v.INFO.get('SVTYPE') == 'TRA'],
+             bins=[i for i in range(0, 2000, 50)])
+    plt.xlabel('Lenghth of structural variant')
+    plt.ylabel('Number of variants')
+    plt.savefig(sys.argv[2])
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
