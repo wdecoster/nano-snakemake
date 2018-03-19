05a62578aacaf5ed34eaf9311c4cef76ac927c2d
wdecoster <decosterwouter@gmail.com>
2018-03-19 16:57:33 +0100

    specify output file name

--- scripts/SV-length-plot.py
+++ scripts/SV-length-plot.py
@@ -7,4 +7,4 @@ plt.hist(x=[v.INFO.get('SVLEN') for v in VCF(sys.argv[1]) if not v.INFO.get('SVT
          bins=[i for i in range(0, 2000, 50)])
 plt.xlabel('Lenghth of structural variant')
 plt.ylabel('Number of variants')
-plt.savefig("SV-length.png")
+plt.savefig(sys.argv[2])
