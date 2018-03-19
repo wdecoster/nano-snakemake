5e314b314e477c62219f6f75abefdfc66851bbc7
wdecoster <decosterwouter@gmail.com>
2018-03-19 16:49:26 +0100

    plotting histogram of length of SVs

---
+++ scripts/SV-length-plot.py
@@ -0,0 +1,10 @@
+from cyvcf2 import VCF
+import sys
+import matplotlib.pyplot as plt
+
+
+plt.hist(x=[v.INFO.get('SVLEN') for v in VCF(sys.argv[1]) if not v.INFO.get('SVTYPE') == 'TRA'],
+         bins=[i for i in range(0, 5000, 50)])
+plt.xlabel('Lenghth of structural variant')
+plt.ylabel('Number of variants')
+plt.savefig("SV-length.png")
