9f645a67cf2b119bcd5885eb730975025f8bc045
wdecoster <decosterwouter@gmail.com>
2018-06-14 15:07:33 +0200

    make stacked barchart of SV-lengths

--- scripts/SV-length-plot.py
+++ scripts/SV-length-plot.py
@@ -2,16 +2,26 @@
 
 from cyvcf2 import VCF
 import sys
+from collections import defaultdict
+import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
-def main(vcf):
-    plt.hist(x=[v.INFO.get('SVLEN') for v in VCF(vcf) if not v.INFO.get('SVTYPE') == 'TRA'],
-             bins=[i for i in range(0, 2000, 50)])
+def main(vcf_file):
+    len_dict = defaultdict(list)
+    for v in VCF(vcf_file):
+        if not v.INFO.get('SVTYPE') == 'TRA':
+            len_dict[v.INFO.get('SVTYPE')].append(v.INFO.get('SVLEN'))
+    plt.hist(x=np.array(list(len_dict.values())),
+             bins=[i for i in range(0, 2000, 50)],
+             stacked=True,
+             histtype='bar',
+             label=list(len_dict.keys()))
     plt.xlabel('Lenghth of structural variant')
     plt.ylabel('Number of variants')
+    plt.legend()
     plt.savefig(sys.argv[2])
 
 
