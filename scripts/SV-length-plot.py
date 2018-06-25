860f657a13b685c37189b830b1996a923df7cfc3
wdecoster <decosterwouter@gmail.com>
2018-06-25 14:55:35 +0200

    additional length plot with bigger bins up to 20kb

--- scripts/SV-length-plot.py
+++ scripts/SV-length-plot.py
@@ -14,14 +14,30 @@ def main(vcf_file):
     for v in VCF(vcf_file):
         if not v.INFO.get('SVTYPE') == 'TRA':
             len_dict[v.INFO.get('SVTYPE')].append(v.INFO.get('SVLEN'))
-    plt.hist(x=np.array(list(len_dict.values())),
+    lengths = np.array(list(len_dict.values()))
+    plt.subplot(2, 1, 1)
+    plt.hist(x=lengths,
              bins=[i for i in range(0, 2000, 50)],
              stacked=True,
              histtype='bar',
              label=list(len_dict.keys()))
     plt.xlabel('Lenghth of structural variant')
     plt.ylabel('Number of variants')
-    plt.legend()
+    plt.legend(frameon=False,
+               fontsize="small")
+
+    plt.subplot(2, 1, 2)
+    plt.hist(x=lengths,
+             bins=[i for i in range(0, 20000, 500)],
+             stacked=True,
+             histtype='bar',
+             label=list(len_dict.keys()),
+             log=True)
+    plt.xlabel('Lenghth of structural variant')
+    plt.ylabel('Number of variants')
+    plt.legend(frameon=False,
+               fontsize="small")
+    plt.tight_layout()
     plt.savefig(sys.argv[2])
 
 
