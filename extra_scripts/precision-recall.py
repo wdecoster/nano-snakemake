87b1c12d6547b2f887b3365adab169e8d7659ea5
wdecoster <decosterwouter@gmail.com>
2018-07-13 13:38:09 +0200

    add minlength argument and change survivor api

--- extra_scripts/precision-recall.py
+++ extra_scripts/precision-recall.py
@@ -9,14 +9,21 @@ from matplotlib_venn import venn2
 
 def main():
     args = get_args()
-    combined_vcf = survivor(args)
+    if args.ignore_type:
+        ignore_type = "-1"
+    else:
+        ignore_type = "1"
+    combined_vcf = survivor(samples=[args.truth, args.test],
+                            distace=args.distance,
+                            ignore_type=ignore_type,
+                            minlength=args.minlength)
     truth_set, test_set = make_venn(combined_vcf)
     tp = len(truth_set & test_set)
     print(f"Precision: {round(100 * tp / len(test_set), ndigits=1)}%")
     print(f"Recall: {round(100 * tp / len(truth_set), ndigits=1)}%")
 
 
-def survivor(args):
+def survivor(samples, distance, ignore_type, minlength):
     """
     Executes SURVIVOR merge, with parameters:
     -samples.fofn (truth and test)
@@ -25,18 +32,14 @@ def survivor(args):
     -require variants to have sampe type (args.ignore_type)
     -require variants to be on same strand (no)
     -estimate distance between calls (no)
-    -specify minimal size of SV event (no)
+    -specify minimal size of SV event (args.minlength)
     """
-    samples = "samples.fofn"
+    fofn_f = "samples.fofn"
     vcf_out = "combined.vcf"
-    with open(samples, 'w') as fofn:
-        fofn.write(args.truth + "\n")
-        fofn.write(args.test + "\n")
-    if args.ignore_type:
-        ignore_type = "-1"
-    else:
-        ignore_type = "1"
-    survivor_cmd = f"SURVIVOR merge {samples} {args.distance} 1 {ignore_type} -1 -1 -1 {vcf_out}"
+    with open(fofn_f, 'w') as fofn:
+        for s in samples:
+            fofn.write(s + "\n")
+    survivor_cmd = f"SURVIVOR merge {fofn_f} {distance} 1 {ignore_type} -1 -1 {minlength} {vcf_out}"
     sys.stderr.write("Executing SURVIVOR...\n")
     subprocess.call(shlex.split(survivor_cmd))
     return VCF(vcf_out)
@@ -69,6 +72,9 @@ def get_args():
     parser.add_argument("-d", "--distance",
                         help="maximum distance between test and truth call",
                         default=500)
+    parser.add_argument("--minlength",
+                        help="Minimum length of SVs to be taken into account",
+                        default=-1)
     parser.add_argument("-i", "--ignore_type",
                         help="Ignore the type of the structural variant",
                         action="store_true")
